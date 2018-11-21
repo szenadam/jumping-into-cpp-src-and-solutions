4c744544be0d12b61c073df6c80e7bd849611884
szenadam <szenadam1@gmail.com>
2018-11-21 19:01:29 +0100

    Add for loop to ch05 01

--- ch05/sample-codes/01.main.cpp
+++ ch05/sample-codes/01.main.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main ()
 {
     int i = 0;
-    while ( i < 10 )
-    {
+    while ( i < 10 ) {
         cout << i << '\n';
         i++;
+    }
+
+    for ( int i = 0; i < 10; i++ ) {
+        cout << i << '\n';
     }
 
     return 0;
