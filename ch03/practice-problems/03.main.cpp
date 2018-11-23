fc87ca56f7bf3567076bdb5cf8e2cb8af2c124a8
szenadam <szenadam1@gmail.com>
2018-11-23 15:44:36 +0100

    Add ch03 03 practice problem

---
+++ ch03/practice-problems/03.main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double n, m;
+
+    cout << "Enter number: ";
+    cin >> n;
+    cout << "Enter number: ";
+    cin >> m;
+
+    cout << n << " divided by " << m << " = " << n / m;
+    return 0;
+}
