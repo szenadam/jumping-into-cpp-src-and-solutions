b4a7303a94696e95fae9abfa669ea544c985695b
szenadam <szenadam1@gmail.com>
2018-11-23 15:44:30 +0100

    Add ch03 02 practice problem

---
+++ ch03/practice-problems/02.main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    cout << "Enter number: ";
+    cin >> n;
+    cout << "Enter number: ";
+    cin >> m;
+
+    cout << "Sum: " << n+m << endl;
+    return 0;
+}
