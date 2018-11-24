1bcefc3c26d1896ddd468483d96102fa5c1da9eb
szenadam <szenadam1@gmail.com>
2018-11-24 17:10:26 +0100

    Add ch04 04 practice problem

---
+++ ch04/practice-problems/04.main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string username, password;
+
+    cout << "Enter username: "; cin >> username;
+    cout << "Enter password: "; cin >> password;
+
+    if ((username == "foo" && password == "bar") ||
+        (username == "baz" && password == "baz"))
+    {
+        cout << "Correct" << endl;
+    } else {
+        cout << "Incorect password or username" << endl;
+    }
+
+    return 0;
+}
